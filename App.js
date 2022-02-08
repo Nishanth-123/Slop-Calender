92c9e599fc8724de4dd50799293021b1bfe9fee8
Nishanth-123 <nishanthmekala39@gmail.com>
2022-02-08 21:53:11 +0530

    initial files

--- App.js
+++ App.js
@@ -1,20 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import Red_Calendar_With_Date from './components/Red_Calendar_With_Date';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Red_Calendar_With_Date/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
